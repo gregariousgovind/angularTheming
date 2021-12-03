2297c46f6c4346c479c765f11bc0c6057d29c1a1
gregariousgovind <govind.chauhan@mountblue.io>
2021-12-04 03:52:25 +0530

    5 files modified

--- src/app/components/carousal/carousal.component.ts
+++ src/app/components/carousal/carousal.component.ts
@@ -6,7 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carousal.component.scss'],
 })
 export class CarousalComponent implements OnInit {
+  private content;
+  private cardWidth = 250; // Should be same as --cardWidth;
+  private scrollStep;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.scrollStep = this.cardWidth + 30;
+    this.content = document.getElementById('cardWrapperId');
+  }
+
+  scrollLeft(e) {
+    e.preventDefault();
+    let sl = this.content.scrollLeft;
+
+    if (sl - this.scrollStep <= 0) {
+      this.content.scrollTo({ left: 0, behaviour: 'smooth' });
+    } else {
+      this.content.scrollTo({
+        left: sl - this.scrollStep,
+        behaviour: 'smooth',
+      });
+    }
+  }
+
+  scrollRight(e) {
+    e.preventDefault();
+    let sl = this.content.scrollLeft,
+      cw = this.content.scrollWidth;
+
+    if (sl + this.scrollStep >= cw) {
+      this.content.scrollTo({ left: cw, behaviour: 'smooth' });
+    } else {
+      this.content.scrollTo({
+        left: sl + this.scrollStep,
+        behaviour: 'smooth',
+      });
+    }
+  }
 }
