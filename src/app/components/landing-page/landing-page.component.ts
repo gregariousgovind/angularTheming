7685d888d1ccf5d981fad2720ee51db17d9fc6c5
gregariousgovind <govind.chauhan@mountblue.io>
2021-12-05 03:18:45 +0530

    3 files modified

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IButton } from '../button/button.model';
 
 @Component({
   selector: 'app-landing-page',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing-page.component.scss'],
 })
 export class LandingPageComponent implements OnInit {
+  public resumeBtnConfig: IButton;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.resumeBtnConfig = {
+      title: 'View My Resume',
+    };
+  }
 }
