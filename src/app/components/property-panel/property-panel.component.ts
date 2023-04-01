e4ce1469c0a750e38f2c2899b5f7742653de36b0
gregariousgovind <48236750+gregariousgovind@users.noreply.github.com>
2023-04-01 13:40:03 +0530

    1 file modified, 2 added

--- src/app/components/property-panel/property-panel.component.ts
+++ src/app/components/property-panel/property-panel.component.ts
@@ -1,128 +1,113 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'property-panel',
-  template: `
-    <form [formGroup]="form" (ngSubmit)="onSubmit()">
-      <ul *ngIf="!collapsed">
-        <li *ngFor="let key of keys">
-          <label [attr.for]="key">{{ key }}</label>
-          <ng-container [ngSwitch]="getTypeof(obj[key])">
-            <ng-container *ngSwitchCase="'string'">
-              <input type="text" [formControlName]="key" [ngClass]="inputClasses" [id]="key">
-            </ng-container>
-            <ng-container *ngSwitchCase="'number'">
-              <input type="number" [formControlName]="key" [ngClass]="inputClasses" [id]="key">
-            </ng-container>
-            <ng-container *ngSwitchCase="'boolean'">
-              <input type="checkbox" [formControlName]="key" [ngClass]="inputClasses" [id]="key">
-            </ng-container>
-            <ng-container *ngSwitchCase="'object'">
-              <ng-container *ngIf="isArray(obj[key])">
-                <select [formControlName]="key" [ngClass]="inputClasses" [id]="key">
-                  <option *ngFor="let item of obj[key]" [value]="item">{{ item }}</option>
-                </select>
-              </ng-container>
-              <ng-container *ngIf="!isArray(obj[key])">
-                <li>
-                  <input type="checkbox" [(ngModel)]="expanded" [id]="'toggle-' + key" [name]="'toggle-' + key" class="toggle-input">
-                  <label [for]="'toggle-' + key" class="toggle-label">{{ key }}</label>
-                  <ul *ngIf="expanded">
-                    <li>
-                      <property-panel [obj]="obj[key] || {}" [form]="form.get(key)" [inputClasses]="inputClasses"></property-panel>
-                    </li>
-                  </ul>
-                </li>
-              </ng-container>
-            </ng-container>
-            <ng-container *ngSwitchDefault>
-              <input type="text" [formControlName]="key" [ngClass]="inputClasses" [id]="key">
-            </ng-container>
-          </ng-container>
-        </li>
-      </ul>
-      <div *ngIf="keys.length === 0">No inputs to display</div>
-      <button type="submit" [disabled]="form.invalid">Submit</button>
-    </form>
-  `,
-  styles: [
-    `
-    .toggle-label {
-      cursor: pointer;
-    }
-    .toggle-input {
-      display: none;
-    }
-    .toggle-input:checked ~ ul {
-      display: block;
-    }
-  `,
-  ],
+  selector: 'app-property-panel',
+  templateUrl: './property-panel.component.html',
+  styleUrls: ['./property-panel.component.css']
 })
 export class PropertyPanelComponent {
-  @Input() obj: object = {};
-  @Input() inputClasses: string;
-  @Output() formSubmit: EventEmitter<any> = new EventEmitter();
+  @Input() data: any;
+  @Output() updated = new EventEmitter<any>();
 
   form: FormGroup;
-  expanded = true;
-  collapsed = false;
 
-  get keys(): string[] {
-    return Object.keys(this.obj);
-  }
-
-  isArray(value: any): boolean {
-    return Array.isArray(value);
-  }
-
-  constructor(private fb: FormBuilder) {
-    this.form = this.fb.group({});
-  }
+  constructor() { }
 
   ngOnInit() {
-    this.keys.forEach((key) => {
-      let value = this.obj[key];
-      let control = null;
-
-      if (typeof value === 'string') {
-        control = this.fb.control(key, [
-          Validators.required,
-          Validators.minLength(3),
-          Validators.maxLength(50),
-          Validators.pattern(/^[A-Za-z]+$/),
-        ]);
-      } else if (typeof value === 'number') {
-        control = this.fb.control(key, [
-          Validators.required,
-          Validators.min(0),
-          Validators.max(100),
-        ]);
-      } else if (typeof value === 'boolean') {
-        control = this.fb.control(key);
-      } else if (Array.isArray(value)) {
-        control = this.fb.control(key);
-      } else if (typeof value === 'object') {
-        control = this.fb.group({});
-        Object.keys(value).forEach((k) => {
-          control.addControl(k, this.fb.control(value[k]));
-        });
-      } else {
-        control = this.fb.control(key);
-      }
-
-      this.form.addControl(key, control);
-    });
+    this.form = this.createForm(this.data);
   }
 
   onSubmit() {
-    if (this.form.valid) {
-      this.formSubmit.emit(this.form.value);
+    this.updated.emit(this.form.value);
+  }
+
+  createForm(data: any): FormGroup {
+    const formGroup = new FormGroup({});
+
+    for (const key of Object.keys(data)) {
+      const value = data[key];
+      let formControl: FormControl;
+
+      switch (this.getType(value)) {
+        case 'string':
+          formControl = new FormControl(value);
+          break;
+        case 'number':
+          formControl = new FormControl(value);
+          break;
+        case 'boolean':
+          formControl = new FormControl(value);
+          break;
+        case 'object':
+          formControl = new FormControl(this.createForm(value));
+          break;
+        case 'array':
+          formControl = new FormControl(value);
+          break;
+      }
+
+      formGroup.addControl(key, formControl);
+    }
+
+    return formGroup;
+  }
+
+  getType(val: any): string {
+    if (typeof val === 'string') {
+      return 'string';
+    } else if (typeof val === 'number') {
+      return 'number';
+    } else if (typeof val === 'boolean') {
+      return 'boolean';
+    } else if (Array.isArray(val)) {
+      return 'array';
+    } else {
+      return 'object';
     }
   }
 
-  getTypeof(value) {
-    return typeof value;
+  objectKeys(obj: any): string[] {
+    return Object.keys(obj);
   }
+
+  toggleExpand(key: string) {
+    this.isExpandedMap[key] = !this.isExpandedMap[key];
+  }
+
+  isExpanded(key: string): boolean {
+    return this.isExpandedMap[key];
+  }
+
+  getOptions(arr: any[]): string[] {
+    if (arr.length === 0) {
+      return [];
+    } else if (typeof arr[0] === 'string') {
+      return arr;
+    } else {
+      return Object.keys(arr[0]);
+    }
+  }
+
+  isObject(val: any): boolean {
+    return typeof val === 'object';
+  }
+
+  isPrimitive(val: any): boolean {
+    return !this.isObject(val) && !Array.isArray(val);
+  }
+
+  isFormControl(val: any): boolean {
+    return val instanceof FormControl;
+  }
+
+  isFormGroup(val: any): boolean {
+    return val instanceof FormGroup;
+  }
+
+  isFormArray(val: any): boolean {
+    return val instanceof FormArray;
+  }
+
+  isExpandedMap: { [key: string]: boolean } = {};
 }
