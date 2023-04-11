158dbc6f1b368a01b812dfb66acba9a500a9a599
gregariousgovind <48236750+gregariousgovind@users.noreply.github.com>
2023-04-12 02:52:14 +0530

    3 files modified

--- src/app/components/component-list/component-list.component.ts
+++ src/app/components/component-list/component-list.component.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-component-list',
-  templateUrl: './component-list.component.html',
-  styleUrls: ['./component-list.component.scss'],
-})
-export class ComponentListComponent {
-  selectedComponentIndex: number = -1;
+interface ComponentItem {
+  name: string;
+  icon: string;
+}
 
-  selectComponent(index: number) {
-    this.selectedComponentIndex = index;
+@Component({
+  selector: 'app-component-panel',
+  templateUrl: './component-panel.component.html',
+  styleUrls: ['./component-panel.component.scss'],
+})
+export class ComponentPanelComponent {
+  components: ComponentItem[] = [
+    { name: 'Component 1', icon: 'M16 18.2V5.8L7.6 12z' },
+    { name: 'Component 2', icon: 'M4 4h16v4H4zm0 6h16v4H4zm0 6h16v4H4z' },
+    { name: 'Component 3', icon: 'M16 18.2V5.8L7.6 12z' },
+    { name: 'Component 4', icon: 'M4 4h16v4H4zm0 6h16v4H4zm0 6h16v4H4z' },
+    { name: 'Component 5', icon: 'M16 18.2V5.8L7.6 12z' },
+    { name: 'Component 6', icon: 'M4 4h16v4H4zm0 6h16v4H4zm0 6h16v4H4z' },
+  ];
+
+  selectedComponent: ComponentItem = null;
+  isCollapsed = true;
+
+  selectComponent(component: ComponentItem) {
+    this.selectedComponent = component;
+  }
+
+  toggleCollapse() {
+    this.isCollapsed = !this.isCollapsed;
   }
 }
