96ec139479693dd70f9d852e6dbf4e8d0ff1150d
gregariousgovind <48236750+gregariousgovind@users.noreply.github.com>
2023-04-12 03:04:45 +0530

    3 files modified

--- src/app/components/component-list/component-list.component.ts
+++ src/app/components/component-list/component-list.component.ts
@@ -12,22 +12,28 @@ interface ComponentItem {
 })
 export class ComponentListComponent {
   components: ComponentItem[] = [
-    { name: 'Component 1', icon: 'M16 18.2V5.8L7.6 12z' },
-    { name: 'Component 2', icon: 'M4 4h16v4H4zm0 6h16v4H4zm0 6h16v4H4z' },
-    { name: 'Component 3', icon: 'M16 18.2V5.8L7.6 12z' },
-    { name: 'Component 4', icon: 'M4 4h16v4H4zm0 6h16v4H4zm0 6h16v4H4z' },
-    { name: 'Component 5', icon: 'M16 18.2V5.8L7.6 12z' },
-    { name: 'Component 6', icon: 'M4 4h16v4H4zm0 6h16v4H4zm0 6h16v4H4z' },
+    {
+      name: 'Component 1',
+      icon: 'M6 4h12v4h-4v10H6V8H2V4h4zm4 6v8h8v-8h-2v6h-4v-6h-2z',
+    },
+    {
+      name: 'Component 2',
+      icon: 'M19 11h-5V6h-4v5H5v8h14v-8h1v-5zm-7 5h-2v2h2v-2z',
+    },
+    {
+      name: 'Component 3',
+      icon: 'M17 16h-2V9h2v7zm-3 0h-2V7h2v9zm-3 0H9V11h2v5zm-3 0H6V5h2v11zm-3 0H3V13h2v3z',
+    },
   ];
 
-  selectedComponent: ComponentItem = null;
-  isCollapsed = true;
-
-  selectComponent(component: ComponentItem) {
-    this.selectedComponent = component;
-  }
+  selectedComponent: string;
+  isCollapsed = false;
 
   toggleCollapse() {
     this.isCollapsed = !this.isCollapsed;
+  }
+
+  select(componentName: string) {
+    this.selectedComponent = componentName;
   }
 }
