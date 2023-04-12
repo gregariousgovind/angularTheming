fde3b3a16c22d69e3dab8d1bcc7e7c091f87e0e0
gregariousgovind <48236750+gregariousgovind@users.noreply.github.com>
2023-04-12 20:46:01 +0530

    2 files modified, 1 added

---
+++ src/app/components/component-list/icon.ts
@@ -0,0 +1,21 @@
+export const icon = `
+  <svg style="position: absolute; width: 0; height: 0;" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <defs>
+      <symbol id="icon_global-loader" viewBox="0 0 24 24">
+        <circle cx="12" cy="12" r="10" fill="none" stroke="#bbbbbb" stroke-width="2"/>
+        <path d="M 12 2 A 10 10 0 0 1 22 12" stroke="#007aff" stroke-width="2" fill="none" stroke-linecap="round">
+          <animateTransform attributeName="transform" attributeType="XML" type="rotate" from="0 12 12" to="360 12 12" dur="1s" repeatCount="indefinite"/>
+        </path>
+      </symbol>
+      <symbol id="icon_button" viewBox="0 0 20 20">
+        <path d="M10 2 L18 10 L14 10 M14 10 L14 18 L6 18 M6 18 L6 10 L2 10 L10 2" stroke="#bbbbbb" stroke-width="2" stroke-linecap="round" fill="none" transform="rotate(-45, 10, 10)"/>
+      </symbol>
+      <symbol id="icon_notification" viewBox="0 0 24 24">
+        <path fill="#bbbbbb" d="M20.5 16.5h-17c-.3 0-.5-.2-.5-.5s.2-.5.5-.5h17c.3 0 .5.2.5.5s-.2.5-.5.5zM17 12.5v-6c0-3.3-2.7-6-6-6s-6 2.7-6 6v6l-2.5 2.5v1.5h19v-1.5z"/>
+      </symbol>
+      <symbol id="icon_input-select" viewBox="0 0 20 20">
+        <path d="M10 2 L18 10 L14 10 M14 10 L14 18 L6 18 M6 18 L6 10 L2 10 L10 2" stroke="#bbbbbb" stroke-width="2" stroke-linecap="round" fill="none" transform="rotate(-45, 10, 10)"/>
+      </symbol>
+    </defs>
+  </svg>
+`;
